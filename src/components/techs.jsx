dbdba2ce154d5f0554da8730be8db49d48683fc3
Raziel-Borja <blackrex903@gmail.com>
2023-08-18 01:49:23 -0600

    PUSH

---
+++ src/components/techs.jsx
@@ -0,0 +1,78 @@
+export default () => {
+
+    const features = [
+        {
+            icon:
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+        </svg>,
+            title: "ARDUINO",
+            desc: "We used ARDUINO IDE to programm an Arduino UNO hardware."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                </svg>,
+            title: "Express.js",
+            desc: "This was the framework that we used to the web development (If you have the chance to work with it, waste it)."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                </svg>,
+            title: "MongoDB",
+            desc: "We used this not relational DB cluster to store the data in the cloud."
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                </svg>,
+            title: "Android Studio",
+            desc: "This was the IDE that we used to programm the mobile app."
+        }
+    ]
+
+    return (
+        <section className="py-14">
+            <div className="max-w-screen-xl mx-auto px-4 text-white gap-16 justify-between md:px-8 lg:flex">
+                <div>
+                    <div className="max-w-xl space-y-3">
+                        <h3 className="text-white font-semibold">
+                            Technologies
+                        </h3>
+                        <p className="text-white text-3xl font-semibold sm:text-4xl">
+                            This were the main tools used to develop vibes.
+                        </p>
+                    </div>
+                    <div className="mt-12 max-w-lg lg:max-w-none">
+                        <ul className="space-y-8">
+                            {
+                                features.map((item, idx) => (
+                                    <li key={idx} className="flex gap-x-4">
+                                        <div className="flex-none w-12 h-12 bg-indigo-50 text-indigo-600 rounded-lg flex items-center justify-center">
+                                            {item.icon}
+                                        </div>
+                                        <div>
+                                            <h4 className="text-lg text-white font-semibold">
+                                                {item.title}
+                                            </h4>
+                                            <p className="mt-3">
+                                                {item.desc}
+                                            </p>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+                <div className="mt-12 lg:mt-0">
+                    <img src="https://res.cloudinary.com/floatui/image/upload/v1670150563/desktop_dte2ar.png" className="w-full shadow-lg rounded-lg border" />
+                </div>
+            </div>
+        </section>
+    )
+}
